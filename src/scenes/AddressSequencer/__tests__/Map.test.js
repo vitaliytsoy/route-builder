b6f5a098c0a2d25ab217a8d559e1187d82673f08
Vitaliy <vitaliy.cva.socio@gmail.com>
2018-11-23 13:55:06 +0600

    Fix fake data names for tests

--- src/scenes/AddressSequencer/__tests__/Map.test.js
+++ src/scenes/AddressSequencer/__tests__/Map.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Map from '../components/Map'
+import Map from '../components/Map/Map';
 
 it('renders without crashing with addressList prop provided', () => {
     shallow(<Map />);
--- src/scenes/AddressSequencer/__tests__/_fakeData.js
+++ src/scenes/AddressSequencer/__tests__/_fakeData.js
@@ -2,7 +2,7 @@ let fakeAdderessesList = [
     {
         formatted_address: "Moscow, Russia",
         id: "4ae974c0-eec2-11e8-b8ce-9b18e7fd4a36",
-        geomerty: {
+        geometry: {
             location: {
                 lat: () => 55.52345,
                 lng: () => 43.22355,
@@ -12,7 +12,7 @@ let fakeAdderessesList = [
     {
         formatted_address: "Berlin, Germany",
         id: "5db63b10-eec2-11e8-b8ce-9b18e7fd4a36",
-        geomerty: {
+        geometry: {
             location: {
                 lat: () => 55.52345,
                 lng: () => 43.22355,
@@ -22,7 +22,7 @@ let fakeAdderessesList = [
     {
         formatted_address: "Crete Region, Greece",
         id: "654aff00-eec2-11e8-b8ce-9b18e7fd4a36",
-        geomerty: {
+        geometry: {
             location: {
                 lat: () => 55.52345,
                 lng: () => 43.22355,
@@ -32,7 +32,7 @@ let fakeAdderessesList = [
     {
         formatted_address: "Rostov-on-Don, Rostov Oblast, Russia",
         id: "6a3ae1b0-eec2-11e8-b8ce-9b18e7fd4a36",
-        geomerty: {
+        geometry: {
             location: {
                 lat: () => 55.52345,
                 lng: () => 43.22355,
@@ -41,4 +41,5 @@ let fakeAdderessesList = [
     },
 ];
 
+xit('fake', () => {});
 export {fakeAdderessesList};
